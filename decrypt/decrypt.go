9511177df54de9757ac3a96cf57d1f4fadd51257
Geraint Edwards <geraint.edwards@ons.gov.uk>
2016-12-05 10:46:57 +0000

    update error checking and test accordingly

--- decrypt/decrypt.go
+++ decrypt/decrypt.go
@@ -8,22 +8,25 @@ import (
 	"log"
 )
 
-func DecryptFile(path string, key string) []byte {
+func DecryptFile(path string, key string) ([]byte, error) {
 	aeskey, err := base64.StdEncoding.DecodeString(key)
 	if err != nil {
-		log.Printf("Decrypted key is not base64 encoded")
+		log.Println("Decrypted key is not base64 encoded", err.Error())
+		return nil, err
 	}
 
 	data, err := ioutil.ReadFile(path)
 	if err != nil {
-		log.Printf("Failed to load file at %s", path)
+		log.Println("Failed to load file at", path, err.Error())
+		return nil, err
 	}
 
 	block, err := aes.NewCipher(aeskey)
 	if err != nil {
-		log.Printf("Failed to create cipher with the key provided")
+		log.Println("Failed to create cipher with the key provided", err.Error())
+		return nil, err
 	}
-	return decrypt(block, data)
+	return decrypt(block, data), nil
 }
 
 func decrypt(block cipher.Block, ciphertext []byte) []byte {
--- decrypt/decrypt_test.go
+++ decrypt/decrypt_test.go
@@ -1,18 +1,33 @@
 package decrypt
 
 import (
-	"strings"
 	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
 )
 
 // Only testing the correct pass, if this is moved to production error cases
 // need tested.
-func TestDecyptZebedeeFile(t *testing.T) {
+func TestDecryptCollectionFile(t *testing.T) {
 	testFile := "../test-data/collections/test-0001/complete/about/data.json"
 	key := "2iyOwMI3YF+fF+SDqMlD8Q=="
-	content := DecryptFile(testFile, key)
-	expecting := "We are the UK’s largest independent producer of official statistics"
-	if !strings.Contains(string(content), expecting) {
-		t.Errorf("Decryption failed: %s", string(content))
-	}
+
+	Convey("With a filename and key, test for decryption", t, func() {
+		plaintext, err := DecryptFile(testFile, key)
+		So(err, ShouldBeNil)
+		So(string(plaintext), ShouldContainSubstring, "We are the UK’s largest independent producer of official statistics")
+	})
+
+	Convey("With a good filename, but a bad key, ensure we get failure", t, func() {
+		plaintext, err := DecryptFile(testFile, "not_a_key")
+		So(err, ShouldNotBeNil)
+		So(plaintext, ShouldBeNil)
+	})
+
+	Convey("With a broken filename, ensure we get failure", t, func() {
+		plaintext, err := DecryptFile("/does/not/exist", "not_a_key")
+		So(err, ShouldNotBeNil)
+		So(plaintext, ShouldBeNil)
+	})
+
 }
