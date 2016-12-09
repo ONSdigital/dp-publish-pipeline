fdb52420a88597fdcb6f9ee238f3aa1b8743e8c0
Matt <Matt@Matts-Mac-Pro.local>
2016-12-09 16:23:55 +0000

    Publish receiver takes both meta and s3 json messages
    Each message is then stored in its own collection within
    mongodb

--- publish-receiver/publish-receiver.go
+++ publish-receiver/publish-receiver.go
@@ -11,45 +11,91 @@ import (
 )
 
 type DataSet struct {
-	FileLocation string
-	FileContent  string
-	CollectionId string
+	CollectionId string `json:"collectionId"`
+	FileLocation string `json:"fileLocation"`
+	FileContent  string `json:"fileContent"`
+	S3Location   string `json:"s3Location"`
 }
+
+type S3Set struct {
+	CollectionId string `bson:"collectionId"`
+	FileLocation string `bson:"fileLocation"`
+	S3Location   string `bson:"s3Location"`
+}
+
+type MetaSet struct {
+	CollectionId string `bson:"collectionId"`
+	FileLocation string `bson:"fileLocation"`
+	FileContent  string `bson:"fileContent"`
+}
+
+const DATA_BASE = "onswebsite"
+const MASTER_COLLECTION = "master"
+const S3_COLLECTION = "s3"
+
+const MONGODB_ENV = "MONGODB"
+const CONSUMER_TOPIC_ENV = "CONSUME_TOPIC"
 
 func storeData(jsonMessage []byte) {
 	var dataSet DataSet
 	err := json.Unmarshal(jsonMessage, &dataSet)
 	if err != nil {
 		log.Printf("Failed to parse json message")
-	} else if dataSet.CollectionId != "" && dataSet.FileContent != "" && dataSet.FileLocation != "" {
-		session, err := mgo.Dial(utils.GetEnvironmentVariable("MONGODB", "localhost"))
+	} else if dataSet.CollectionId != "" && dataSet.FileLocation != "" {
+		session, err := mgo.Dial(utils.GetEnvironmentVariable(MONGODB_ENV, "localhost"))
 		if err != nil {
 			panic(err)
 		}
 		defer session.Close()
-		collection := session.DB("ONSWebsite").C("master")
-		// Try to update the existing document. The Mongo driver sets keys to be
-		// all lower case.
-		query := bson.M{"filelocation": dataSet.FileLocation}
-		change := bson.M{"$set": bson.M{"filecontent": dataSet.FileContent, "collecionid": dataSet.CollectionId}}
-		updateErr := collection.Update(query, change)
-		if updateErr != nil {
-			// No document existed so this must be a new page
-			insertError := collection.Insert(dataSet)
-			if insertError != nil {
-				panic(insertError)
-			}
-			log.Printf("Inserted page at %s", dataSet.FileLocation)
+		db := session.DB(DATA_BASE)
+		if dataSet.FileContent != "" {
+			collection := db.C(MASTER_COLLECTION)
+			metaContent(*collection, dataSet)
+		} else if dataSet.S3Location != "" {
+			collection := db.C(S3_COLLECTION)
+			s3Content(*collection, dataSet)
 		} else {
-			log.Printf("Updated page at %s", dataSet.FileLocation)
+			log.Printf("Unknown data from %v", dataSet)
 		}
+	}
+}
+
+func s3Content(collection mgo.Collection, dataSet DataSet) {
+	query := bson.M{"fileLocation": dataSet.FileLocation}
+	change := bson.M{"$set": bson.M{"s3Location": dataSet.S3Location, "collecionId": dataSet.CollectionId}}
+	updateErr := collection.Update(query, change)
+	if updateErr != nil {
+		// No document existed so this must be a new resource
+		s3Set := S3Set{dataSet.CollectionId, dataSet.FileLocation, dataSet.S3Location}
+		insertError := collection.Insert(s3Set)
+		if insertError != nil {
+			panic(insertError)
+		}
+		log.Printf("Inserted resource at %s", dataSet.FileLocation)
 	} else {
-		log.Printf("Json message missing fields : %s", string(jsonMessage))
+		log.Printf("Updated resource at %s", dataSet.FileLocation)
+	}
+}
+
+func metaContent(collection mgo.Collection, dataSet DataSet) {
+	query := bson.M{"fileLocation": dataSet.FileLocation}
+	change := bson.M{"$set": bson.M{"fileContent": dataSet.FileContent, "collecionId": dataSet.CollectionId}}
+	updateErr := collection.Update(query, change)
+	if updateErr != nil {
+		// No document existed so this must be a new page
+		metaSet := MetaSet{dataSet.CollectionId, dataSet.FileLocation, dataSet.FileContent}
+		insertError := collection.Insert(metaSet)
+		if insertError != nil {
+			panic(insertError)
+		}
+		log.Printf("Inserted page at %s", dataSet.FileLocation)
+	} else {
+		log.Printf("Updated page at %s", dataSet.FileLocation)
 	}
 }
 
 func main() {
-	topic := utils.GetEnvironmentVariable("TOPIC", "test")
+	topic := utils.GetEnvironmentVariable(CONSUMER_TOPIC_ENV, "uk.gov.ons.dp.web.complete-file")
 	master := kafka.CreateConsumer()
 	log.Printf("Started publish receiver on topic '%s'", topic)
 	defer func() {
