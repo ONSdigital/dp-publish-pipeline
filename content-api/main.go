7ae0d652395c36ee144992ff526ea2b122e3c942
Mathew Guest <guesm@C02SV3TCG8WL.local>
2017-02-16 15:29:59 +0000

    Updated content-api to use postgres datastore
    All metadata is fetched from postgres
    Static files are lookup in postgres then fetched from S3

--- content-api/main.go
+++ content-api/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"database/sql"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"strings"
 
 	"github.com/ONSdigital/dp-publish-pipeline/utils"
+	_ "github.com/lib/pq"
 )
+
+var findMetaDataStatement *sql.Stmt
+var findS3DataStatement *sql.Stmt
 
 func exportHandler(w http.ResponseWriter, r *http.Request) {
 	generatorURL := utils.GetEnvironmentVariable("GENERATOR_URL", "localhost:8092")
@@ -34,8 +39,29 @@ func generatorHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write(body)
 }
 
+func prepareSQLStatement(sql string, db *sql.DB) *sql.Stmt {
+	statement, err := db.Prepare(sql)
+	if err != nil {
+		log.Panicf("Error: Could not prepare statement on database: %s", err.Error())
+	}
+	return statement
+}
+
 func main() {
+	dbSource := utils.GetEnvironmentVariable("DB_ACCESS", "user=dp dbname=dp sslmode=disable")
 	port := utils.GetEnvironmentVariable("PORT", "8082")
+	db, err := sql.Open("postgres", dbSource)
+	if err != nil {
+		log.Panicf("DB open error: %s", err.Error())
+	}
+	defer db.Close()
+	findMetaDataSQL := "SELECT content FROM metadata WHERE uri = $1"
+	findS3DataSQL := "SELECT s3 FROM s3data WHERE uri = $1"
+	findMetaDataStatement = prepareSQLStatement(findMetaDataSQL, db)
+	findS3DataStatement = prepareSQLStatement(findS3DataSQL, db)
+	defer findMetaDataStatement.Close()
+	defer findS3DataStatement.Close()
+
 	log.Printf("Starting Content API on port : %s", port)
 	http.HandleFunc("/data", getData)
 	http.HandleFunc("/parent", getParent)
@@ -43,6 +69,5 @@ func main() {
 	http.HandleFunc("/taxonomy", getTaxonomy)
 	http.HandleFunc("/generator", generatorHandler)
 	http.HandleFunc("/export", exportHandler)
-
 	http.ListenAndServe(":"+port, nil)
 }
