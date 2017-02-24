751a0f822cadbbf126047f7114ade82c7f83e0e1
Geraint Edwards <geraint.edwards@ons.gov.uk>
2017-02-24 14:58:25 +0000

    minor improvements to upload script

--- scripts/createZebedeeCollection.sh
+++ scripts/createZebedeeCollection.sh
@@ -22,33 +22,33 @@ read -r -d '' collectionJson <<- EOM
 EOM
 
 # Get args
-while [[ $# -gt 1 ]]
-do
-key="$1"
+while [[ $# -gt 1 ]]; do
+    key="$1"
 
-case $key in
-    -d|--directory)
-    DIRECTORY="$2"
-    shift # past argument
-    ;;
-    -t|--access_token)
-    TOKEN="$2"
-    shift # past argument
-    ;;
-    -z|--zebedee_root)
-    ZEBEDEE_ROOT="$2"
-    shift # past argument
-    ;;
-    *)
-            # unknown option
-    ;;
-esac
-shift # past argument or value
+    case $key in
+        -d|--directory)
+            DIRECTORY="$2"
+            shift # past argument
+            ;;
+        -t|--access_token)
+            TOKEN="$2"
+            shift # past argument
+            ;;
+        -z|--zebedee_root)
+            ZEBEDEE_ROOT="$2"
+            shift # past argument
+            ;;
+        *)
+            echo Bad arg: $key >&2
+            exit 2 # unknown option
+            ;;
+    esac
+    shift # past argument or value
 done
-echo DIRECTORY  = "${DIRECTORY}"
-echo Token     = "${TOKEN}"
+echo "DIRECTORY = $DIRECTORY"
+echo "Token     = $TOKEN"
 
-# traveres collections
+# traverse collections
 filesToSend=$(find ${DIRECTORY} -type f)
 
 # Create collection
@@ -60,29 +60,26 @@ response=$(curl -sb -X POST --cookie "access_token=${TOKEN}" \
 # Find the collectionID in the json message. This is needed to add content
 # to the collection. (We also need to remove the quotes from the string)
 collectionID=$(echo $response | jq '.id')
-collectionID="${collectionID%\"}"
-collectionID="${collectionID#\"}"
+collectionID=${collectionID%"}
+collectionID=${collectionID#"}
 
 # Add content to the collection
-for file in ${filesToSend[@]}
-do
+for file in ${filesToSend[@]}; do
   uri=${file/${DIRECTORY}/}
-  if [[ $string == *".json"* ]]; then
-    curlUri=$host"/zebedee/content/"$collectionID"?uri="$uri"&overwriteExisting=true&recursive=undefined"
-  else
-    curlUri=$host"/zebedee/content/"$collectionID"?uri="$uri"&overwriteExisting=true&recursive=undefined&validateJson=false"
+  curlUri=$host"/zebedee/content/"$collectionID"?uri="$uri"&overwriteExisting=true&recursive=undefined"
+  if [[ $string != *".json" ]]; then
+    curlUri=$curlUri"&validateJson=false"
   fi
   fileAdded=$(curl -sb -X POST --cookie "access_token=${TOKEN}" $curlUri -d @$file)
-  if [ "$fileAdded" == "true" ]
-  then
+  if [[ "$fileAdded" == "true" ]]; then
     echo "Added $uri to collection"
   else
-    echo "Failed to add $uri to collection"
+    echo "Failed to add $uri to collection: $fileAdded"
   fi
 done
 
 inprogressDirectory=${ZEBEDEE_ROOT}"zebedee/collections/"${collectionID%-*}"/inprogress/*"
 completeDirectory=${ZEBEDEE_ROOT}"zebedee/collections/"${collectionID%-*}"/reviewed"
-echo "Moving content to complete"
+echo "Moving content to complete $completeDirectory"
 mv $inprogressDirectory $completeDirectory
 echo "Collection ready to be approved"
