6931996e5a8c9632db0bd567c60f89503273b29e
Geraint Edwards <geraint.edwards@ons.gov.uk>
2017-02-27 13:41:35 +0000

    add concurrency (of curl jobs) into the script

--- scripts/createZebedeeCollection.sh
+++ scripts/createZebedeeCollection.sh
@@ -1,6 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Notes : This needs jq installed use `brew install jq`
+
+log() { echo $(date '+%Y-%m-%d %T') "$@"; }
+testing=
+max_running=20
 
 # A random name for the collection
 collectionName=$(uuidgen)
@@ -34,9 +38,16 @@ while [[ $# -gt 1 ]]; do
             HOST="$2"
             shift
             ;;
+        -p|--parallel)
+            max_running=$2
+            shift # past argument
+            ;;
         -z|--zebedee_root)
             ZEBEDEE_ROOT="$2"
             shift # past argument
+            ;;
+        --test)
+            testing=1
             ;;
         *)
             echo Bad arg: $key >&2
@@ -45,8 +56,9 @@ while [[ $# -gt 1 ]]; do
     esac
     shift # past argument or value
 done
-echo "DIRECTORY = $DIRECTORY"
-echo "Token     = $TOKEN"
+echo "Collection = $collectionName"
+echo "DIRECTORY  = $DIRECTORY"
+echo "Token      = $TOKEN"
 
 # traverse collections
 filesToSend=$(find ${DIRECTORY} -type f)
@@ -54,10 +66,12 @@ filesToSend=$(find ${DIRECTORY} -type f)
 # Create collection
 echo "Creating Collection : $collectionName"
 
-response=$(curl -sb -X  POST --cookie "access_token=${TOKEN}" \
- $HOST"/zebedee/collection" -d "$collectionJson" -k)
+if [[ -z $testing ]]; then
+  response=$(curl -sb -X  POST --cookie "access_token=${TOKEN}" \
+        $HOST"/zebedee/collection" -d "$collectionJson" -k)
+fi
 
-echo $response
+log Response: $response
 # Find the collectionID in the json message. This is needed to add content
 # to the collection. (We also need to remove the quotes from the string)
 collectionID=$(echo $response | jq '.id')
@@ -65,22 +79,52 @@ collectionID="${collectionID%\"}"
 collectionID="${collectionID#\"}"
 
 # Add content to the collection
+running=0
+run_seq=0
+error_count=0
 for file in ${filesToSend[@]}; do
   uri=${file/${DIRECTORY}/}
   curlUri=$HOST"/zebedee/content/"$collectionID"?uri="$uri"&overwriteExisting=true&recursive=undefined"
   if [[ $string != *".json" ]]; then
     curlUri=$curlUri"&validateJson=false"
   fi
-  fileAdded=$(curl -sb -X POST --cookie "access_token=${TOKEN}" $curlUri -d @$file -k)
-  if [[ "$fileAdded" == "true" ]]; then
-    echo "Added $uri to collection"
-  else
-    echo "Failed to add $uri to collection: $fileAdded"
+  if (( running >= max_running )); then
+    wait -n; res=$?; let running--; [[ $res != 0 ]] && let error_count++
   fi
+  {
+    if [[ -z $testing ]]; then
+      fileAdded=$(curl -sb -X POST --cookie "access_token=${TOKEN}" $curlUri -d @$file -k)
+    else
+      sleep 1.$run_seq
+      fileAdded=true
+      if [[ $run_seq == *[248] ]]; then
+        fileAdded=$run_seq  # simulate failure for some jobs
+      fi
+    fi
+    if [[ "$fileAdded" != "true" ]]; then
+      log "[$run_seq] Failed to add $uri to collection: $fileAdded"
+      exit 2
+    fi
+    log "[$run_seq] Added $uri to collection"
+  } &
+  let running++
+  let run_seq++
+done
+while (( running > 0 )); do
+  wait -n; res=$?
+  [[ $res != 0 ]] && let error_count++
+  let running--
 done
 
 inprogressDirectory=${ZEBEDEE_ROOT}"zebedee/collections/"${collectionID%-*}"/inprogress/*"
 completeDirectory=${ZEBEDEE_ROOT}"zebedee/collections/"${collectionID%-*}"/reviewed"
-echo "Moving content to complete $completeDirectory"
-mv $inprogressDirectory $completeDirectory
-echo "Collection ready to be approved"
+if [[ -z $testing ]]; then
+  echo "Moving content to complete $completeDirectory"
+  mv $inprogressDirectory $completeDirectory
+fi
+if [[ $error_count -gt 0 ]]; then
+  echo "Collection had $error_count errors"
+  exit $error_count
+else
+  echo "Collection ready to be approved"
+fi
