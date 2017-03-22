a80d4a2778b836084151ffe9f7c5e0b0bbac2e59
Geraint Edwards <geraint.edwards@ons.gov.uk>
2017-03-22 11:40:54 +0000

    add DecryptS3(...) for decrypting s3 (not file) assets

--- decrypt/decrypt.go
+++ decrypt/decrypt.go
@@ -8,12 +8,42 @@ import (
 	"io"
 	"log"
 	"os"
+
+	"github.com/ONSdigital/dp-publish-pipeline/s3"
 )
 
-func DecryptFile(path string, key string) ([]byte, error) {
+func decodeKey(key string) (cipher.Block, error) {
 	aeskey, err := base64.StdEncoding.DecodeString(key)
 	if err != nil {
 		log.Println("Decrypted key is not base64 encoded", err.Error())
+		return nil, err
+	}
+	block, err := aes.NewCipher(aeskey)
+	if err != nil {
+		log.Println("Failed to create cipher with the key provided", err.Error())
+		return nil, err
+	}
+	return block, nil
+}
+
+func DecryptS3(s3Client s3.S3Client, path string, key string) ([]byte, error) {
+	block, err := decodeKey(key)
+	if err != nil {
+		return nil, err
+	}
+
+	reader, err := s3Client.GetReader(path)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer reader.Close()
+
+	return decrypt(block, reader, path), nil
+}
+
+func DecryptFile(path string, key string) ([]byte, error) {
+	block, err := decodeKey(key)
+	if err != nil {
 		return nil, err
 	}
 
@@ -23,19 +53,14 @@ func DecryptFile(path string, key string) ([]byte, error) {
 		return nil, err
 	}
 
-	block, err := aes.NewCipher(aeskey)
-	if err != nil {
-		log.Println("Failed to create cipher with the key provided", err.Error())
-		return nil, err
-	}
-	return decrypt(block, inFile), nil
+	return decrypt(block, inFile, path), nil
 }
 
-func decrypt(block cipher.Block, inFile *os.File) []byte {
+func decrypt(block cipher.Block, inFile io.Reader, path string) []byte {
 	// The IV will be the first BlockSize bytes of inFile
 	iv := make([]byte, block.BlockSize())
 	if _, err := io.ReadFull(inFile, iv); err != nil {
-		panic(err)
+		log.Panicf("Failed to read %q - %s", path, err)
 	}
 	stream := cipher.NewCTR(block, iv)
 	reader := &cipher.StreamReader{S: stream, R: inFile}
@@ -43,7 +68,7 @@ func decrypt(block cipher.Block, inFile *os.File) []byte {
 	plaintextBuf := bytes.NewBuffer(nil)
 	_, err := io.Copy(plaintextBuf, reader)
 	if err != nil {
-		panic(err)
+		log.Panicf("Failed to copy %q - %s", path, err)
 	}
 
 	return plaintextBuf.Bytes()
