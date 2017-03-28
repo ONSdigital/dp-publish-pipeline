f674b4a1e87dd7854519a7e175b1cfa1f7d6f936
Mathew Guest <guesm@C02SV3TCG8WL.local>
2017-03-28 14:28:08 +0100

    Fixed issues elastic search indices for 5.2

--- publish-search-indexer/createIndex.sh
+++ publish-search-indexer/createIndex.sh
@@ -3,11 +3,11 @@
 elasticUri='http://localhost:9200'
 
 echo "Creating ONS Index"
-curl -w "\n" -XPUT $elasticUri'/ons' -d @ons-index/index-settings.json
+curl -w "\n" -XPUT $elasticUri'/ons/' -d @ons-index/index-settings.json
 echo "Creating ONS default mappings"
 curl -w "\n" -XPUT $elasticUri'/ons/_default_/_mapping' -d @ons-index/default-mapping.json
 echo "Creating departments Index"
-curl -w "\n" -XPUT $elasticUri'/departments' -d @departments-index/index-settings.json
+curl -w "\n" -XPUT $elasticUri'/departments/' -d @departments-index/index-settings.json
 echo "Creating departments default mappings"
 curl -w "\n" -XPUT $elasticUri'/departments/_default_/_mapping' -d @departments-index/default-mapping.json
 
