c811db6d57e1dd802a99bee25265060a95dc80ba
Geraint Edwards <geraint.edwards@ons.gov.uk>
2017-04-27 10:48:25 +0100

    make scripts more consistent with env vars from other repos

--- publish-search-indexer/createIndex.sh
+++ publish-search-indexer/createIndex.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-elasticUri='http://localhost:9200'
+ELASTIC_URL=${ELASTIC_URL:-http://localhost:9200}
 
 echo "Creating ONS Index"
-curl -w "\n" -XPUT $elasticUri'/ons/' -d @ons-index/index-settings.json
+curl -w "\n" -XPUT $ELASTIC_URL'/ons/' -d @ons-index/index-settings.json
 echo "Creating ONS default mappings"
-curl -w "\n" -XPUT $elasticUri'/ons/_default_/_mapping' -d @ons-index/default-mapping.json
+curl -w "\n" -XPUT $ELASTIC_URL'/ons/_default_/_mapping' -d @ons-index/default-mapping.json
 echo "Creating departments Index"
-curl -w "\n" -XPUT $elasticUri'/departments/' -d @departments-index/index-settings.json
+curl -w "\n" -XPUT $ELASTIC_URL'/departments/' -d @departments-index/index-settings.json
 echo "Creating departments default mappings"
-curl -w "\n" -XPUT $elasticUri'/departments/_default_/_mapping' -d @departments-index/default-mapping.json
+curl -w "\n" -XPUT $ELASTIC_URL'/departments/_default_/_mapping' -d @departments-index/default-mapping.json
 
-jsonFiles=$(find departments-index/data -type f | grep ".json")
+jsonFiles=$(find departments-index/data -type f -name "*.json")
 
 for file in ${jsonFiles[@]}
 do
   id=${file:23:${#file}-28}
   echo "Creating document $id"
-  curl -w "\n" -XPUT $elasticUri'/departments/departments/'$id -d @$file
+  echo curl -w "\n" -XPUT $ELASTIC_URL'/departments/departments/'$id -d @$file
 done
--- publish-search-indexer/deleteIndex.sh
+++ publish-search-indexer/deleteIndex.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-elasticUri='http://localhost:9200'
+
+ELASTIC_URL=${ELASTIC_URL:-http://localhost:9200}
+
 echo "Deleting ONS index"
-curl -w "\n" -XDELETE $elasticUri'/ons'
+curl -w "\n" -XDELETE $ELASTIC_URL'/ons'
 echo "Deleting Departments index"
-curl -w "\n" -XDELETE $elasticUri'/departments'
+curl -w "\n" -XDELETE $ELASTIC_URL'/departments'
