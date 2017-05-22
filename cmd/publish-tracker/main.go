367cbdc162157740589f4f9f0fd83dcafea43549
Geraint Edwards <geraint.edwards@ons.gov.uk>
2017-05-22 14:45:21 +0100

    add /healthcheck to tracker

--- cmd/publish-tracker/main.go
+++ cmd/publish-tracker/main.go
@@ -5,8 +5,10 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"net/http"
 	"time"
 
+	"github.com/ONSdigital/dp-publish-pipeline/health"
 	"github.com/ONSdigital/dp-publish-pipeline/kafka"
 	"github.com/ONSdigital/dp-publish-pipeline/utils"
 	"github.com/ONSdigital/go-ns/log"
@@ -106,6 +108,8 @@ func main() {
 	log.Namespace = "publish-tracker"
 	completeFileTopic := utils.GetEnvironmentVariable("COMPLETE_FILE_FLAG_TOPIC", "uk.gov.ons.dp.web.complete-file-flag")
 	completeCollectionTopic := utils.GetEnvironmentVariable("COMPLETE_TOPIC", "uk.gov.ons.dp.web.complete")
+	healthCheckAddr := utils.GetEnvironmentVariable("HEALTHCHECK_ADDR", ":8080")
+	healthCheckEndpoint := utils.GetEnvironmentVariable("HEALTHCHECK_ENDPOINT", "/healthcheck")
 	maxConcurrentFileCompletes, err := utils.GetEnvironmentVariableInt("MAX_CONCURRENT_FILE_COMPLETES", 40)
 	if err != nil {
 		log.ErrorC("Cannot convert MAX_CONCURRENT_FILE_COMPLETES to integer", err, nil)
@@ -128,6 +132,7 @@ func main() {
 	dbMeta.prep("update-completed-file", "UPDATE schedule_file SET complete_time=$2 WHERE schedule_file_id=$1")
 	dbMeta.prep("update-delete-file", "UPDATE schedule_delete SET complete_time=$2 WHERE schedule_delete_id=$1")
 	dbMeta.prep("update-complete-job", "UPDATE schedule SET complete_time=$2 WHERE schedule_id=$1 AND start_time IS NOT NULL AND complete_time IS NULL RETURNING collection_id, start_time")
+	dbMeta.prep("healthcheck", "SELECT 1 FROM schedule_delete")
 
 	fileConsumer, err := kafka.NewConsumerGroup(completeFileTopic, "publish-tracker")
 	if err != nil {
@@ -137,12 +142,20 @@ func main() {
 	producer := kafka.NewProducer(completeCollectionTopic)
 
 	rateLimitFileCompletes := make(chan bool, maxConcurrentFileCompletes)
+	healthChannel := make(chan bool)
 
 	go func() {
 		tock := time.Tick(tick)
 		for _ = range tock {
 			checkForCompletedJobs(dbMeta, producer)
 		}
+	}()
+
+	go func() {
+		http.HandleFunc(healthCheckEndpoint, health.NewHealthChecker(healthChannel, dbMeta.prepped["healthcheck"]))
+		log.Info(fmt.Sprintf("Listening for %s on %s", healthCheckEndpoint, healthCheckAddr), nil)
+		log.ErrorC("healthcheck listener exited", http.ListenAndServe(healthCheckAddr, nil), nil)
+		panic("healthcheck listened exited")
 	}()
 
 	for {
@@ -157,6 +170,7 @@ func main() {
 		case errorMessage := <-fileConsumer.Errors:
 			log.Error(errors.New("Aborting after consumer error"), log.Data{"msg": errorMessage})
 			panic("Aborting after consumer error")
+		case <-healthChannel:
 		}
 	}
 }
